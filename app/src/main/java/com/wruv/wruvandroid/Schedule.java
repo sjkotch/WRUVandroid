8be94c47428123d0d8659d540539ef1f139460a4
sjkotch <sjkotch@uvm.edu>
2018-10-15 20:43:35 -0400

    [ADD] Added onCreate method for all activities with corresponding XMLs

--- app/src/main/java/com/wruv/wruvandroid/Schedule.java
+++ app/src/main/java/com/wruv/wruvandroid/Schedule.java
@@ -1,9 +1,14 @@
 package com.wruv.wruvandroid;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class Schedule extends AppCompatActivity {
 
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+    }
 
 }
