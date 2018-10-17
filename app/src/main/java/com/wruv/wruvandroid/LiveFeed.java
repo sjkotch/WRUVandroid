611db5595f3831f1f27a98e3732b211a1901159f
sjkotch <sjkotch@uvm.edu>
2018-10-16 21:01:45 -0400

    [ADD] Added cell XMLs to be used in the live feed

--- app/src/main/java/com/wruv/wruvandroid/LiveFeed.java
+++ app/src/main/java/com/wruv/wruvandroid/LiveFeed.java
@@ -5,10 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 
 public class LiveFeed extends AppCompatActivity {
 
+    //XML elements
+
+
+    //Variables
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live_feed);
     }
+
+
+
 
 }
