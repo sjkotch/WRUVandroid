379452eea9537b518954fdb149a231d17b058d2e
Leslie M VanDeMark <leslie.vandemark@uvm.edu>
2018-10-30 11:06:04 -0400

    added android calendar view

--- app/src/main/java/com/wruv/wruvandroid/Schedule.java
+++ app/src/main/java/com/wruv/wruvandroid/Schedule.java
@@ -1,14 +1,23 @@
 package com.wruv.wruvandroid;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.CalendarView;
+
+//        GET http://wruv.creek.fm/api/schedule;
 
 public class Schedule extends AppCompatActivity {
+
+    CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+        calendarView = (CalendarView) findViewById(R.id.calendarLayout);
+
+
     }
 
 }
