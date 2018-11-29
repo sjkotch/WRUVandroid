6e8244c7c87da03858fd49c1c888e45548fb17a1
Leslie M VanDeMark <leslie.vandemark@uvm.edu>
2018-11-28 20:50:23 -0500

    added Parse and Back4app keys

--- app/src/main/java/com/wruv/wruvandroid/Schedule.java
+++ app/src/main/java/com/wruv/wruvandroid/Schedule.java
@@ -10,6 +10,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 
+import com.parse.GetCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
 //        GET http://wruv.creek.fm/api/schedule;
 
 public class Schedule extends AppCompatActivity {
@@ -37,6 +43,28 @@ public class Schedule extends AppCompatActivity {
                 Intent intent = new Intent(Schedule.this, ScheduleDay.class);
                 intent.putExtra("date", date);
                 startActivity(intent);
+            }
+        });
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.back4app_app_id))
+                // if defined
+                .clientKey(getString(R.string.back4app_client_key))
+                .server(getString(R.string.back4app_server_url))
+                .build()
+        );
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Schedule");
+        query.getInBackground("sj69I01ndC", new GetCallback<ParseObject>() {
+            public void done(ParseObject object, ParseException e) {
+                if (e == null) {
+                    // object will be your game score
+                    String playerName = object.getString("dj");
+                    Log.d(TAG,"onQuery " + playerName);
+
+                } else {
+                    // something went wrong
+                }
             }
         });
 
