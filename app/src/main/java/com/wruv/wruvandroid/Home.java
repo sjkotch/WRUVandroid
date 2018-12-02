2acb7f97352eca80665c169ac76324dd592cd9b8
LeAnn Gove <lgove@uvm.edu>
2018-12-02 17:10:32 -0500

    [ADD]basic music player UI

--- app/src/main/java/com/wruv/wruvandroid/Home.java
+++ app/src/main/java/com/wruv/wruvandroid/Home.java
@@ -1,15 +1,28 @@
 package com.wruv.wruvandroid;
 
 import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-import static android.content.Intent.FLAG_ACTIVITY_NO_ANIMATION;
+import android.os.Handler;
+
+import android.widget.TextView;
+
+
 
 public class Home extends AppCompatActivity {
 
+    private String audioFile;
+    private Handler handler = new Handler();
+    private boolean pCurrentlyPlaying = false;
+
+
+    public static final String AUDIO_FILE_NAME = "C:/Users/leann/AndroidStudioProjects/WRUVandroid/MusicFolder/Marias_IDontKnowYou.mp3";
+    String[] tokens = AUDIO_FILE_NAME.split(".+?/(?=[^/]+$)");
+    String songName = tokens[1];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +32,18 @@ public class Home extends AppCompatActivity {
         ImageButton navLiveFeed = (ImageButton) findViewById(R.id.navLiveFeed);
         ImageButton navSchedule = (ImageButton) findViewById(R.id.navSchedule);
         ImageButton navChat = (ImageButton) findViewById(R.id.navChat);
+
+        final ImageButton playStop = (ImageButton) findViewById(R.id.playStop);
+        playStop.setImageResource(R.drawable.play);
+
+
+        this.getIntent().putExtra(AUDIO_FILE_NAME,AUDIO_FILE_NAME);
+        audioFile = this.getIntent().getStringExtra(songName);
+        ((TextView)findViewById(R.id.now_playing_text)).setText(songName);
+
+
+
+
 
 //        navStream.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -51,5 +76,19 @@ public class Home extends AppCompatActivity {
                 overridePendingTransition(0,0);
             }
         });
+
+        playStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int image = pCurrentlyPlaying ? R.drawable.stop : R.drawable.play;
+                playStop.setImageResource(image);
+                pCurrentlyPlaying = !pCurrentlyPlaying ;
+
+            }
+        });
+
+
     }
+
+
 }
